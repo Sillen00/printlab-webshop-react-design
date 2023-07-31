1da169c0d814e04d7ae2be03614930c9c3bbbc28
Simon <sillen.00@hotmail.com>
2023-07-31 21:39:41 +0200

    header component with html setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import { useState } from 'react'
 
+import Header from "./components/header";
+
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <header>
+      <img src="#" alt="Printlab Logo" />
+      <div>
+        <ul>
+          <li>HOME</li>
+          <li>SHOP</li>
+          <li>ABOUT</li>
+        </ul>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+          </svg>
+        </div>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M5 22h14c1.103 0 2-.897 2-2V9a1 1 0 0 0-1-1h-3V7c0-2.757-2.243-5-5-5S7 4.243 7 7v1H4a1 1 0 0 0-1 1v11c0 1.103.897 2 2 2zM9 7c0-1.654 1.346-3 3-3s3 1.346 3 3v1H9V7zm-4 3h2v2h2v-2h6v2h2v-2h2l.002 10H5V10z"></path>
+          </svg>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
