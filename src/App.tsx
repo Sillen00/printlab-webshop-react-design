08b5c85137844fd1d0c53fd6ce8808f6015d2bc9
Simon <sillen.00@hotmail.com>
2023-08-01 22:13:38 +0200

    Added 2 a home and shop page with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-// import { useState } from 'react'
-
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 }
