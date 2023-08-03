6cfbc26d6fc94bb44eac9d414fdc692411d800f3
Simon <sillen.00@hotmail.com>
2023-08-03 10:20:26 +0200

    some more subhead styling + html

--- src/components/Subhead.tsx
+++ src/components/Subhead.tsx
@@ -3,13 +3,20 @@ import "./Subhead.css";
 function Subhead() {
   return (
     <div className="subhead-div">
-      <div>
+      <div className="subhead-image-div">
         <img
+            className="subhead-image"
           src="./assets/subhead-image.jpg"
           alt="Subhead image of a printer making a design ball where you can put a tea light in."
         />
       </div>
-      <div>subhead text</div>
+      <div className="subhead-text-div">
+        <h2>Unleash Your Imagination With 3D Printing! </h2>
+        <p>Transform your ideas into reality, Welcome to PrintLab!</p>
+
+        
+
+      </div>
     </div>
   );
 }
