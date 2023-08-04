29568d8f3881065ef298f6b133e17ba1909894fe
Simon <sillen.00@hotmail.com>
2023-08-04 22:37:41 +0200

    Wy shop with us section done with media queries and css.

--- src/components/homepage-components/WhyShopWithUs.tsx
+++ src/components/homepage-components/WhyShopWithUs.tsx
@@ -6,17 +6,12 @@ function WhyShopWithUs() {
       <h2 className="WSWU-h2">Why Shop With Us</h2>
       <div className="WSWU-div">
         <div className="WSWU-card">
-          <img
-            className="WSWU-images"
-            src="./assets/benefit1.png"
-            alt="black and white image on a 3d printer machine"
-          />
+          <img src="./assets/benefit1.png" alt="black and white image on a 3d printer machine" />
           <h6>Unparalleled Customization</h6>
           <p>Create a one-of-a-kind item!</p>
         </div>
         <div className="WSWU-card">
           <img
-            className="WSWU-images"
             src="./assets/benefit2.png"
             alt="close up image of a 3d printer making a crown like product."
           />
@@ -28,7 +23,6 @@ function WhyShopWithUs() {
         </div>
         <div className="WSWU-card">
           <img
-            className="WSWU-images"
             src="./assets/benefit3.png"
             alt="close up image on a 3d printed sqware that standing on one of it edges."
           />
@@ -40,7 +34,6 @@ function WhyShopWithUs() {
         </div>
         <div className="WSWU-card">
           <img
-            className="WSWU-images"
             src="./assets/benefit4.png"
             alt="image showing printlab have ecofriendly product with a mini fan and a leaf underneath it."
           />
