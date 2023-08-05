36b3c36f05b308240e009138ff114f7381c6dd7e
Simon <sillen.00@hotmail.com>
2023-08-05 15:41:49 +0200

    Removing 2 images. Will replace them

--- src/components/homepage-components/Products.tsx
+++ src/components/homepage-components/Products.tsx
@@ -10,9 +10,29 @@ function Products() {
             <OrangeButton>Create Your Own</OrangeButton>
         </div>
 
-        <div>
-            <img src="" alt="" />
+        <div className="product-card">
+            <img src="./assets/product1.png" alt="3D printed design cube with holes in it." />
             <h6>Design Cube</h6>
+            <p>Add To Cart</p>
+        </div>
+        <div className="product-card">
+            <img src="./assets/product2.png" alt="3D printed tape mesure holder." />
+            <h6>Tape Mesure Holder</h6>
+            <p>Add To Cart</p>
+        </div>
+        <div className="product-card">
+            <img src="./assets/product3.png" alt="3D printed self-watering planter with a plant in it." />
+            <h6>Self-Watering Planter</h6>
+            <p>Add To Cart</p>
+        </div>
+        <div className="product-card">
+            <img src="./assets/product4.png" alt="3D printed Whistle Blower that makes over 118 decibel." />
+            <h6>Whistle Blower</h6>
+            <p>Add To Cart</p>
+        </div>
+        <div className="product-card">
+            <img src="./assets/product5.png" alt="3D printed Pikachu figure that is holding a fishingrow, fishing in a tea cup." />
+            <h6>Pikachu Tea Holder</h6>
             <p>Add To Cart</p>
         </div>
     </section>
