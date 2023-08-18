0242bd296dfb64e01f0eeee4433cf94256ec761d
Simon <sillen.00@hotmail.com>
2023-08-18 11:18:51 +0200

    saved all files

--- src/components/homepage-components/Products.tsx
+++ src/components/homepage-components/Products.tsx
@@ -5,49 +5,49 @@ function Products() {
   return (
     <section className="product-section">
       <div className="product-card">
-          <h2>
-            Our Top <br />
-            Bought Products:
-          </h2>
-          <p>
-            Create a one-of-a-kind <br /> product!
-          </p>
-          <OrangeButton>Create Your Own</OrangeButton>
+        <h2>
+          Our Top <br />
+          Bought Products:
+        </h2>
+        <p>
+          Create a one-of-a-kind <br /> product!
+        </p>
+        <OrangeButton>Create Your Own</OrangeButton>
       </div>
 
       <div className="product-card">
-          <img src="./assets/product1.png" alt="3D printed design cube with holes in it." />
-          <h6>Design Cube</h6>
-          <p>Add To Cart</p>
+        <img src="./assets/product1.png" alt="3D printed design cube with holes in it." />
+        <h6>Design Cube</h6>
+        <p>Add To Cart</p>
       </div>
       <div className="product-card">
-          <img src="./assets/product2.png" alt="3D printed tape mesure holder." />
-          <h6>Tape Mesure Holder</h6>
-          <p>Add To Cart</p>
+        <img src="./assets/product2.png" alt="3D printed tape mesure holder." />
+        <h6>Tape Mesure Holder</h6>
+        <p>Add To Cart</p>
       </div>
       <div className="product-card">
-          <img
-            src="./assets/product3.png"
-            alt="3D printed self-watering planter with a plant in it."
-          />
-          <h6>Self-Watering Planter</h6>
-          <p>Add To Cart</p>
+        <img
+          src="./assets/product3.png"
+          alt="3D printed self-watering planter with a plant in it."
+        />
+        <h6>Self-Watering Planter</h6>
+        <p>Add To Cart</p>
       </div>
       <div className="product-card">
-          <img
-            src="./assets/product4.png"
-            alt="3D printed Whistle Blower that makes over 118 decibel."
-          />
-          <h6>Whistle Blower</h6>
-          <p>Add To Cart</p>
+        <img
+          src="./assets/product4.png"
+          alt="3D printed Whistle Blower that makes over 118 decibel."
+        />
+        <h6>Whistle Blower</h6>
+        <p>Add To Cart</p>
       </div>
       <div className="product-card">
-          <img
-            src="./assets/product5.png"
-            alt="3D printed Pikachu figure that is holding a fishingrow, fishing in a tea cup."
-          />
-          <h6>Pikachu Tea Holder</h6>
-          <p>Add To Cart</p>
+        <img
+          src="./assets/product5.png"
+          alt="3D printed Pikachu figure that is holding a fishingrow, fishing in a tea cup."
+        />
+        <h6>Pikachu Tea Holder</h6>
+        <p>Add To Cart</p>
       </div>
     </section>
   );
