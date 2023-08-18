a1fca428190e4b21cfca52a9d25788d573f4d030
Simon <sillen.00@hotmail.com>
2023-08-18 12:58:40 +0200

    vite config css setting test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Output directory for production build
+    outDir: "dist",
+  },
+  css: {
+    // Include or import CSS files globally
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/global.scss";`, // Replace with your global styles path
+      },
+    },
+  },
 });
